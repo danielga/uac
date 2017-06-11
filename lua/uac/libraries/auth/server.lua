46401b2a20e397802afe1c5ec2341f373400d591
Daniel <danielga@outlook.pt>
2017-06-11 16:36:03 +0100

    Fixed automatic setting of roles on player join.

--- lua/uac/libraries/auth/server.lua
+++ lua/uac/libraries/auth/server.lua
@@ -2,10 +2,12 @@ AddCSLuaFile("shared.lua")
 include("shared.lua")
 
 uac.auth = uac.auth or {
-	users = {}
+	users = {},
+	validated = {}
 }
 
 local users_list = uac.auth.users
+local valid_list = uac.auth.validated
 
 hook.Remove("PlayerInitialSpawn", "PlayerAuthSpawn") -- we don't want stuff to break right?
 
@@ -25,17 +27,29 @@ hook.Add("PlayerAuthed", "uac.auth.GameHost", function(ply)
 	if ply:IsGameHost() then
 		ply:SetRole("superadmin")
 	end
+
+	local steamid = ply:SteamID()
+	if valid_list[steamid] then
+		ply:SetRole(users_list[steamid])
+	end
 end)
 
 hook.Add("NetworkIDValidated", "uac.auth.UpdateRole", function(name, steamid)
 	local role = users_list[steamid]
 	if role ~= nil then
+		valid_list[steamid] = true
+
 		local ply = uac.player.GetPlayerFromSteamID(steamid)
 		if ply ~= nil then
 			ply:SetRole(role)
 		end
 	end
 end)
+
+hook.Add("player_disconnect", "uac.auth.CancelUpdateRole", function(data)
+	valid_list[data.networkid] = nil
+end)
+gameevent.Listen("player_disconnect")
 
 hook.Add("Initialize", "uac.auth.LoadUsers", function()
 	uac.auth.LoadUsersList()
